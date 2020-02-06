5327f7cf623e4bb76323264ff0786ee2dd82a8dd
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-02-06 10:54:35 +0200

    age-calculator

--- age_calculator.py
+++ age_calculator.py
@@ -1,28 +1,30 @@
 from datetime import datetime
 
 def check_birthdate(year, month, day):
-    birthdate = datetime(year, month, day)
-    today = datetime.now()
-    if birthdate > today:
-        return False
-    else:
-        return True
+	birthdate = datetime(year, month, day)
+	today = datetime.now()
+	if birthdate > today:
+		return False
+	else:
+		return True
 
 def calculate_age(year, month, day):
-    birthdate = datetime(year, month, day)
-    today = datetime.now()
-    days = (todayBirthdate).days
-    print("You are {} years old.".format(int(days)/365))
+	birthdate = datetime(year, month, day)
+	today = datetime.now()
+	deff = today - birthdate
+	ageInDays = deff.days
+	age = int(ageInDays/365)
+	print("You are {} years old ".format(age))
 
 def main():
-    year = int(input("Enter year of birth: "))
-    month = int(input("Enter month of birth: "))
-    day = int(input("Enter day of birth: "))
+	year = int(input("Enter year of birth:"))
+	month = int(input("Enter month of birth:"))
+	day = int(input("Enter day of birth:"))
 
-    if check_birthdate(year, month, day):
-        calculate_age(year, month, day)
-    else:
-        print("Invalid birthdate!")
+	if check_birthdate(year, month, day):
+		calculate_age(year, month, day)
+	else:
+		print("Invalid birthdate!")
 
 if __name__ == '__main__':
-    main()
+	main()
